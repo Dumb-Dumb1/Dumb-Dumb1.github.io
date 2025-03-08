998b7b7e8b23f08f1a84498b48fff826ec35771d
Dumb-Dumb1 <desktopc24@gmail.com>
2025-03-08 10:21:44 -0800

    image

--- gallery_script.js
+++ gallery_script.js
@@ -1,5 +1,5 @@
 const imageGallery = [
-    "",
-    "",
-    ""
-]
+    "https://cdn.pixabay.com/photo/2016/10/25/12/28/cheese-1762866_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2017/12/09/08/18/cheese-3005572_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2015/05/24/15/33/cheese-781654_960_720.jpg"
+];
