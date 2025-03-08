1aa6d4637eba8338f7c8e0625ef989c08b380714
Dumb-Dumb1 <desktopc24@gmail.com>
2025-03-08 11:15:35 -0800

    image

--- gallery_script.js
+++ gallery_script.js
@@ -1,11 +1,28 @@
 const imageGallery = [
-    "",
-    "",
-    ""
-];
+    "https://images-prod.healthline.com/hlcmsresource/images/AN_images/healthiest-cheese-1296x728-swiss.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/a/a8/Cheese_platter.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Hartkaese_HardCheeses.jpg/1280px-Hartkaese_HardCheeses.jpg"
+]
 
 const imageElement = document.getElementById("gallery-image");
 const prevButton = document.getElementById("prev-btn");
 const nextButton = document.getElementById("next-btn");
 
 let currentImgIndex = 0;
+
+function updateImage() {
+    imageElement.src = imageGallery[currentImgIndex];
+}
+
+nextButton.addEventListener("click", function(){
+    currentImgIndex = (currentImgIndex + 1) % imageGallery.length
+    updateImage()
+});
+
+prevButton.addEventListener("click", function(){
+    currentImgIndex = (currentImgIndex - 1) % imageGallery.length
+    updateImage()
+});
+
+
+updateImage();
